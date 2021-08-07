976fa7095abc01216e511eb0e193e576193af5bf
Christian <hi@lindeneg.org>
2021-08-07 15:51:25 +0200

    describe link tests

--- src/__tests__/link.test.ts
+++ src/__tests__/link.test.ts
@@ -1,42 +1,44 @@
 import { getLink, cleanLinkName, convertCamelCase } from "../link";
 
-test.each([
-  ["/admin/edit/blog/[articleId]", "ADMIN_EDIT_BLOG_ARTICLE_ID"],
-  ["/admin/edit/[slug]/[id]", "ADMIN_EDIT_SLUG_ID"],
-  ["admin/user/[id]", "ADMIN_USER_ID"],
-  ["admin/music/artist/[artistId]", "ADMIN_MUSIC_ARTIST_ARTIST_ID"],
-])("clean: %s => %s", (dirtyLink, cleanLink) => {
-  expect(cleanLinkName(dirtyLink)).toBe(cleanLink);
-});
+describe("Link Test Suite", () => {
+  test.each([
+    ["/admin/edit/blog/[articleId]", "ADMIN_EDIT_BLOG_ARTICLE_ID"],
+    ["/admin/edit/[slug]/[id]", "ADMIN_EDIT_SLUG_ID"],
+    ["admin/user/[id]", "ADMIN_USER_ID"],
+    ["admin/music/artist/[artistId]", "ADMIN_MUSIC_ARTIST_ARTIST_ID"],
+  ])("clean: %s => %s", (dirtyLink, cleanLink) => {
+    expect(cleanLinkName(dirtyLink)).toBe(cleanLink);
+  });
 
-test.each([
-  ["admin/edit/blog/[articleId]", "Admin/edit/blog/[article_Id]"],
-  ["admin/music/artist/[artistId]", "Admin/music/artist/[artist_Id]"],
-  [
-    "adminRoute/music/artist/[artistId]",
-    "Admin_Route/music/artist/[artist_Id]",
-  ],
-])("convert: %s => %s", (before, after) => {
-  expect(convertCamelCase(before)).toBe(after);
-});
+  test.each([
+    ["admin/edit/blog/[articleId]", "Admin/edit/blog/[article_Id]"],
+    ["admin/music/artist/[artistId]", "Admin/music/artist/[artist_Id]"],
+    [
+      "adminRoute/music/artist/[artistId]",
+      "Admin_Route/music/artist/[artist_Id]",
+    ],
+  ])("convert: %s => %s", (before, after) => {
+    expect(convertCamelCase(before)).toBe(after);
+  });
 
-test.each([
-  [
-    "/admin/edit/blog/[articleId].tsx",
-    "/admin/edit/blog/[articleId]",
-    "ADMIN_EDIT_BLOG_ARTICLE_ID",
-  ],
-  [
-    "/admin/edit/[slug]/[id]/index.tsx",
-    "/admin/edit/[slug]/[id]",
-    "ADMIN_EDIT_SLUG_ID",
-  ],
-  ["admin/user/[id]/index.tsx", "/admin/user/[id]", "ADMIN_USER_ID"],
-  [
-    "admin/music/artist/[artistId].tsx",
-    "/admin/music/artist/[artistId]",
-    "ADMIN_MUSIC_ARTIST_ARTIST_ID",
-  ],
-])("link: %s => %s", (rawLink, link, name) => {
-  expect(getLink(rawLink)).toEqual([name, link]);
+  test.each([
+    [
+      "/admin/edit/blog/[articleId].tsx",
+      "/admin/edit/blog/[articleId]",
+      "ADMIN_EDIT_BLOG_ARTICLE_ID",
+    ],
+    [
+      "/admin/edit/[slug]/[id]/index.tsx",
+      "/admin/edit/[slug]/[id]",
+      "ADMIN_EDIT_SLUG_ID",
+    ],
+    ["admin/user/[id]/index.tsx", "/admin/user/[id]", "ADMIN_USER_ID"],
+    [
+      "admin/music/artist/[artistId].tsx",
+      "/admin/music/artist/[artistId]",
+      "ADMIN_MUSIC_ARTIST_ARTIST_ID",
+    ],
+  ])("link: %s => %s", (rawLink, link, name) => {
+    expect(getLink(rawLink)).toEqual([name, link]);
+  });
 });

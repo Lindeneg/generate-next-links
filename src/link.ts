4b45c1bd7ab4f7b52f03f4a54457116023332a6f
Christian <hi@lindeneg.org>
2021-08-07 15:49:33 +0200

    convert camel case

--- src/link.ts
+++ src/link.ts
@@ -1,5 +1,16 @@
 export type Link = [string, string];
 
+export function convertCamelCase(target: string) {
+  const result: string[] = [];
+  target.split(" ").forEach((entry) => {
+    if (entry != "") {
+      let splitWords = entry.split(/(?=[A-Z])/).join("_");
+      result.push(splitWords.charAt(0).toUpperCase() + splitWords.slice(1));
+    }
+  });
+  return result.join(" ");
+}
+
 export function getLink(target: string): Link | null {
   const name = target.replace(/^\//, "").replace(/\.(tsx|jsx)/g, "");
   if (!name || /^(\_app|index)$/.test(name)) {
@@ -13,7 +24,7 @@ export function getLink(target: string): Link | null {
 }
 
 export function cleanLinkName(name: string) {
-  return name
+  return convertCamelCase(name)
     .replace(/^\//, "")
     .replace(/(\/|\-)/g, "_")
     .replace(/\[|\]/g, "")

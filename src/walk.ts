593f95eed996c12be2c17955320811d87f597e93
Christian <hi@lindeneg.org>
2021-08-07 21:32:17 +0200

    generate node tree

--- src/walk.ts
+++ src/walk.ts
@@ -6,6 +6,7 @@ import path from "path";
 type Node = {
   id: number;
   parentId: number | null;
+  children?: Node[];
 };
 
 type NodeMap = Map<Node["id"], string>;
@@ -59,37 +60,67 @@ function walk(
   });
 }
 
-walk("__mock__", (err, results) => {
-  const root: Node = { id: Id.next(), parentId: null };
-  const data: Node[] = [root];
-  const map: NodeMap = new Map();
-  map.set(root.id, "/");
-  const parents: { [k: string]: Node["id"] } = {};
-  results.forEach((result) => {
-    let parentId = root.id;
-    let child = "";
-    const splitted = result.split("pages/");
-    if (splitted.length >= 2) {
-      if (!/^(_app|index)\.tsx$/.test(splitted[1])) {
-        const targets = splitted[1].split("/");
-        child = targets[targets.length - 1].replace(/\.(tsx|jsx)/g, "");
-        if (targets.length > 1) {
-          const parent = targets.slice(0, targets.length - 1).join("/");
-          if (!parents[parent]) {
-            parentId = Id.next();
-            parents[parent] = parentId;
-            map.set(parentId, parent);
-          } else {
-            parentId = parents[parent];
+function generateLinkNodeTree(callback: (nodes: Node[], map: NodeMap) => void) {
+  walk("__mock__", (err, results) => {
+    const root: Node = { id: Id.next(), parentId: null };
+    const nodes: Node[] = [root];
+    const map: NodeMap = new Map();
+    map.set(root.id, "/");
+    const parents: { [k: string]: Node["id"] } = {};
+    results.forEach((result) => {
+      let parentId = root.id;
+      let child = "";
+      const splitted = result.split("pages/");
+      if (splitted.length >= 2) {
+        if (!/^(_app|index)\.tsx$/.test(splitted[1])) {
+          const targets = splitted[1].split("/");
+          child = targets[targets.length - 1].replace(/\.(tsx|jsx)/g, "");
+          if (targets.length > 1) {
+            const parent = targets.slice(0, targets.length - 1);
+            const parentName = parent[parent.length - 1];
+            const parentPath = parent.join("/");
+            if (!parents[parentPath]) {
+              parentId = Id.next();
+              parents[parentPath] = parentId;
+              map.set(parentId, parentName);
+              // push to nodes
+              console.log(parentPath, parentName);
+            } else {
+              parentId = parents[parentPath];
+            }
           }
         }
       }
-    }
-    if (child !== "") {
-      const id = Id.next();
-      map.set(id, child);
-      data.push({ id, parentId });
-    }
+      if (child !== "") {
+        const id = Id.next();
+        map.set(id, child);
+        nodes.push({ id, parentId });
+      }
+    });
+    callback(nodes, map);
   });
-  console.log(data, map);
+}
+
+generateLinkNodeTree((nodes, map) => {
+  const idMapping = nodes.reduce((acc, el, i) => {
+    //@ts-ignore
+    acc[el.id] = i;
+    return acc;
+  }, {});
+  let root;
+  nodes.forEach((el) => {
+    // Handle the root element
+    if (el.parentId === null) {
+      root = el;
+      return;
+    }
+    // Use our mapping to locate the parent element in our data array
+    //@ts-ignore
+    const parentEl = nodes[idMapping[el.parentId]];
+    console.log(el.parentId);
+    console.log(parentEl);
+    // Add our current el to its parent's `children` array
+    //parentEl.children = [...(parentEl.children || []), el];
+  });
+  console.log(root);
 });

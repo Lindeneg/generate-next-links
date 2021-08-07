e5b42429dea3f146ce0e77289c68ed035d0787df
Christian <hi@lindeneg.org>
2021-08-07 15:31:33 +0200

    module tests

--- src/__tests__/module.test.ts
+++ src/__tests__/module.test.ts
@@ -1,1 +1,40 @@
-test("", () => {});
+import { getConfig } from "../config";
+import { main } from "../index";
+
+describe("Module test suite", () => {
+  test("can execute a dry run", () => {
+    const config = getConfig(".", [
+      "",
+      "",
+      "--path",
+      "__mock__",
+      "--dry",
+      "--name",
+      "PageLinks",
+    ]);
+    const result = main(config);
+    expect(result).toEqual([
+      "PageLinks",
+      [
+        ["ADMIN", "/admin"],
+        ["ADMIN_BLOG", "/admin/blog"],
+        ["ADMIN_BLOG_POSTS", "/admin/blog/posts"],
+        ["ADMIN_USER_ID", "/admin/user/[id]"],
+        ["FAQ_LANGUAGE", "/faq/[language]"],
+        ["PRODUCTS_CATEGORY", "/products/[category]"],
+        [
+          "PRODUCTS_CATEGORY_THEME_COLOR_COLORID",
+          "/products/[category]/theme/color/[colorId]",
+        ],
+        [
+          "PRODUCTS_CATEGORY_THEME_CURRENT",
+          "/products/[category]/theme/current",
+        ],
+        ["PRODUCTS_CATEGORY_THEME_NEW", "/products/[category]/theme/new"],
+        ["PRODUCTS_CREATE", "/products/create"],
+        ["PRODUCTS_EDIT", "/products/edit"],
+        ["PRODUCTS_ID", "/products/[id]"],
+      ],
+    ]);
+  });
+});

b9f8943cc448c6e9a3729a8a977932059df2f3a9
Christian <hi@lindeneg.org>
2021-08-08 19:14:55 +0200

    create ts file

--- src/index.ts
+++ src/index.ts
@@ -18,29 +18,27 @@ export function main(
         exit(1);
       }
       const links = getLinks(nodes, map).sort((a, b) => (b[0] > a[0] ? -1 : 1));
-      if (config.dry) {
-        logger(LogLevel.Debug, "dry run chosen, no files committed\n");
-        log(
-          false,
-          LogLevel.Debug,
-          config.name,
-          links.map((link) => link[1]),
-          `\n\ndry run generated ${links.length} links in ${getRunTimeInSeconds(
-            config.start
-          )} seconds`
-        );
-        if (process.env.NODE_ENV === "test") {
-          return callback([config.name, links]);
-        } else {
-          exit(0);
-        }
-      }
       createTsLinksEnum(
-        config.out,
         links,
-        config.name,
-        config.start,
-        config.verbose
+        config,
+        (content) => {
+          logger(LogLevel.Debug, "dry run chosen, no files committed\n");
+          log(
+            false,
+            LogLevel.Debug,
+            config.name,
+            content,
+            `\n\ndry run generated ${
+              links.length
+            } links in ${getRunTimeInSeconds(config.start)} seconds`
+          );
+          if (process.env.NODE_ENV === "test") {
+            return callback([config.name, links]);
+          } else {
+            exit(0);
+          }
+        },
+        logger
       );
     },
     logger

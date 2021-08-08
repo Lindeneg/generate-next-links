c050e357b32244078c7a2dc0a8142266df9d4a57
Christian <hi@lindeneg.org>
2021-08-08 14:47:58 +0200

    get links

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { exit } from "process";
-import { getLinkTree, getLinks } from "./link";
+import { getLinks } from "./link";
+import { getLinkTree } from "./node";
 import { createTsLinksEnum } from "./create";
 import { Config } from "./config";
 import { LogLevel, log, getRunTimeInSeconds } from "./log";
@@ -8,41 +9,37 @@ export function main(config: Config) {
   const logger = log.bind(null, !config.verbose);
   getLinkTree(
     config,
-    (error, root, map) => {
-      if (error || !root || !map) {
+    (error, nodes, map) => {
+      if (error || !nodes || !map) {
         log(false, LogLevel.Error, error || "could not generate link tree");
         exit(1);
       }
-      const links = getLinks(root, map);
+      const links = getLinks(nodes, map).sort((a, b) => (b[0] > a[0] ? -1 : 1));
+      if (config.dry) {
+        logger(LogLevel.Debug, "dry run chosen, no files committed\n");
+        log(
+          false,
+          LogLevel.Debug,
+          config.name,
+          links,
+          `\n\ndry run generated ${links.length} links in ${getRunTimeInSeconds(
+            config.start
+          )} seconds`
+        );
+        if (process.env.NODE_ENV === "test") {
+          return [config.name, links];
+        } else {
+          exit(0);
+        }
+      }
+      createTsLinksEnum(
+        config.out,
+        links,
+        config.name,
+        config.start,
+        config.verbose
+      );
     },
     logger
   );
-  /*
-  logger(LogLevel.Debug, `sorting ${links.length} nextjs links`);
-  const sortedLinks = links.sort((a, b) => (b[0] > a[0] ? -1 : 1));
-  if (config.dry) {
-    logger(LogLevel.Debug, "dry run chosen, no files committed\n");
-    log(
-      false,
-      LogLevel.Debug,
-      config.name,
-      sortedLinks,
-      `\n\ndry run generated ${
-        sortedLinks.length
-      } links in ${getRunTimeInSeconds(start)} seconds`
-    );
-    if (process.env.NODE_ENV === "test") {
-      return [config.name, sortedLinks];
-    } else {
-      exit(0);
-    }
-  }
-  createTsLinksEnum(
-    config.out,
-    sortedLinks,
-    config.name,
-    start,
-    config.verbose
-  );
-  */
 }

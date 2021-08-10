03e9ffdffcda2c99ffe9957d8585b148a22a7564
Christian <christian@lindeneg.org>
2021-08-10 22:45:52 +0200

    bump version

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 import { exit } from "process";
 import { LogLevel, log } from "./log";
 
-const VERSION = "2.0.6";
+const VERSION = "2.0.7";
 
 const HELP = `
 Usage: generate-next-links 

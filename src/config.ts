3e1934616fbb8646cafead091b4b410d3dc3497f
Christian <hi@lindeneg.org>
2021-08-14 00:00:05 +0200

    extend cli

--- src/config.ts
+++ src/config.ts
@@ -11,13 +11,16 @@ If no args are specified, a 'pages' folder must be located
 inside the folder where the script is running from
 
 Options:
- --name      Name of generated TypeScript enum
- --path      Path to folder where 'pages' directory resides
- --out       Path where TypeScript file will be written to
- --dry       Perform all operations except writing to disk
- --verbose   Show all log messages in stdout
- --version   Show current version
- --help      Show help                                                
+ -N --name [NAME]         name of generated TypeScript enum
+ -P --path [PATH]         path to folder where 'pages' directory resides
+ -O --out  [PATH]         path to folder where ts file will be written to
+ -D --dry                 perform all operations except writing to disk
+ -V --verbose             show all log messages in stdout
+ -T --omit-timestamp      omit timestamp from written ts file
+ -J --export-json         export json instead of ts enum
+ -C --convert-camel-case  convert camel case to be delimited by underscore
+ -I --version             show current version
+ -H --help                show help                                                
 
 `;
 
@@ -27,6 +30,9 @@ export type Config = {
   name: string;
   dry: boolean;
   verbose: boolean;
+  omitTimestamp: boolean;
+  exportJson: boolean;
+  convertCamelCase: boolean;
   start: number;
 };
 
@@ -50,6 +56,9 @@ export function getConfig(rootPath: string, args: string[]): Config {
     name: "links",
     dry: false,
     verbose: false,
+    omitTimestamp: false,
+    exportJson: false,
+    convertCamelCase: false,
     start: Date.now(),
   };
   if (args.length >= 3) {
@@ -57,8 +66,11 @@ export function getConfig(rootPath: string, args: string[]): Config {
       const arg = args[i];
       switch (arg) {
         case "--out":
+        case "-O":
         case "--path":
+        case "-P":
         case "--name":
+        case "-N":
           if (i + 1 < args.length) {
             const target =
               (args[i + 1].startsWith("/") || arg === "--name" ? "" : "/") +
@@ -76,10 +88,24 @@ export function getConfig(rootPath: string, args: string[]): Config {
           log(false, LogLevel.Debug, `version: ${VERSION}`);
           exit(0);
         case "--dry":
+        case "-D":
           config.dry = true;
           break;
         case "--verbose":
+        case "-V":
           config.verbose = true;
+          break;
+        case "--omit-timestamp":
+        case "-T":
+          config.omitTimestamp = true;
+          break;
+        case "--convert-camel-case":
+        case "-C":
+          config.convertCamelCase = true;
+          break;
+        case "--export-json":
+        case "-J":
+          config.exportJson = true;
           break;
         case "--help":
           log(false, LogLevel.Debug, HELP);
